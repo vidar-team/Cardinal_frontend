150c35cf35a6724d82dd769c78da45ce8459b57a
John Wu <524306184@qq.com>
2020-02-14 18:30:11 +0800

    add: rank list

--- src/views/Rank.vue
+++ src/views/Rank.vue
@@ -1,12 +1,54 @@
 <template>
-    <div>
-
-    </div>
+    <v-container>
+        <v-simple-table dark v-if="rankList !== null">
+            <thead>
+            <tr>
+                <th class="text-center">排名</th>
+                <th class="text-center">队伍</th>
+                <th class="text-center">分数</th>
+                <th class="text-center" v-bind:key="index" v-for="(header,index) in this.rankHeader">
+                    {{header}}
+                </th>
+            </tr>
+            </thead>
+            <tbody class="text-center">
+            <tr v-for="(item, key) in this.rankList" :key="item.ID">
+                <td>{{key + 1}}</td>
+                <td>{{item.TeamName}}</td>
+                <td>{{item.Score}}</td>
+                <td v-bind:key="index" v-for="(gameBox, index) in item.GameBoxStatus">
+                    <v-icon color="green lighten-2" v-if="!gameBox.IsDown && !gameBox.IsAttacked">mdi-check-circle</v-icon>
+                    <v-icon color="yellow lighten-2" v-if="gameBox.IsDown">mdi-file-cancel</v-icon>
+                    <v-icon color="red lighten-2" v-if="gameBox.IsAttacked">mdi-ghost</v-icon>
+                </td>
+            </tr>
+            </tbody>
+        </v-simple-table>
+        <v-skeleton-loader v-else class="mx-auto" type="article"></v-skeleton-loader>
+    </v-container>
 </template>
 
 <script>
     export default {
-        name: "Rank"
+        name: "Rank",
+
+        data: () => ({
+            rankHeader: null,
+            rankList: null
+        }),
+
+        mounted() {
+            this.getRank()
+        },
+
+        methods: {
+            getRank() {
+                this.utils.GET('/team/rank').then(res => {
+                    this.rankHeader = res.Title
+                    this.rankList = res.Rank
+                })
+            }
+        }
     }
 </script>
 
