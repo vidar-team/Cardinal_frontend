ea580327dbcee44dc9b3ce114c8d0fabf4f54a24
John Wu <524306184@qq.com>
2020-05-06 12:18:35 +0800

    fix: curl command guide

--- src/components/SubmitFlag.vue
+++ src/components/SubmitFlag.vue
@@ -4,14 +4,18 @@
         <v-card-text>
             <h2>POST <code style="background-color: #1c1c1c">/flag</code></h2><br>
             <p>Content-Type: application/json</p>
-            <p>Header:<pre>Authorization: {{info.Token}}</pre></p>
+            <p>Header:
+            <pre>Authorization: {{info.Token}}</pre>
+            </p>
             <p>Body:</p>
-            <code class="pa-3" style="width: 100%; background-color: #1c1c1c; color: rgba(255, 255, 255, 0.7);">{"flag": "your_flag_here"}</code>
+            <code class="pa-3" style="width: 100%; background-color: #1c1c1c; color: rgba(255, 255, 255, 0.7);">{"flag":
+                "your_flag_here"}</code>
             <br><br>
             <v-divider></v-divider>
             <br>
             <p>
-                <code class="pa-3" style="width: 100%; background-color: #1c1c1c; color: rgba(255, 255, 255, 0.7);">curl -X POST {{utils.baseURL}}/flag -H 'Authorization: {{info.Token}}' -d '{ "flag": "your_flag_here" }'</code>
+                <code class="pa-3" style="width: 100%; background-color: #1c1c1c; color: rgba(255, 255, 255, 0.7);">curl
+                    -X POST {{baseURL}}/api/flag -H 'Authorization: {{info.Token}}' -d '{ "flag": "your_flag_here" }'</code>
             </p>
         </v-card-text>
     </v-card>
@@ -22,15 +26,16 @@
         name: "SubmitFlag",
 
         data: () => ({
-            info: null
+            info: null,
+            baseURL: window.location.origin,
         }),
 
         mounted() {
             this.getInfo()
         },
 
-        methods:{
-            getInfo(){
+        methods: {
+            getInfo() {
                 this.utils.GET("/team/info").then(res => {
                     this.info = res
                 })
